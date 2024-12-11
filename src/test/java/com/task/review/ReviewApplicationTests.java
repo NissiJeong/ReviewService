9d9377b4c7e236bb08da08ea511ff66d6e8f1dd5
NissyJeong <jnissi92@gmail.com>
2024-12-11 11:00:21 +0900

    리뷰 프로젝트 init commit

---
+++ src/test/java/com/task/review/ReviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.task.review;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReviewApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
