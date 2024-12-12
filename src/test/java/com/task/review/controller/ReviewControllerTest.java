192b4b6a9c94c27e75223b75129eae4747879668
NissyJeong <jnissi92@gmail.com>
2024-12-12 12:54:27 +0900

    [test] review 등록 유효한 리뷰등록인지 테스트(dto)
    - dto 정확한 스코어 범위인지 테스트

--- src/test/java/com/task/review/controller/ReviewControllerTest.java
+++ src/test/java/com/task/review/controller/ReviewControllerTest.java
@@ -9,11 +9,16 @@ import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDoc
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.nio.charset.StandardCharsets;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.multipart;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(controllers = ReviewController.class)
@@ -30,13 +35,21 @@ class ReviewControllerTest {
 
     @Test
     public void isValidReview() throws Exception {
-        ReviewRequestDto review = new ReviewRequestDto(1L, 5, "content", null);
+        ReviewRequestDto review = new ReviewRequestDto(1L, 7, "content", null);
 
         String reviewJsonString = objectMapper.writeValueAsString(review);
 
-        mvc.perform(post("/products/1/reviews")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(reviewJsonString))
-                .andExpect(status().isBadRequest());
+        System.out.println("reviewJsonString = " + reviewJsonString);
+
+        MockMultipartFile file1 = new MockMultipartFile("image", "empty.txt", "text/plain", "".getBytes());
+        MockMultipartFile file2 = new MockMultipartFile("requestDto", "jsondata", "application/json", reviewJsonString.getBytes(StandardCharsets.UTF_8));
+
+        mvc.perform(multipart("/products/1/reviews")
+                    .file(file1)
+                    .file(file2)
+                    .accept(MediaType.APPLICATION_JSON)
+                    .characterEncoding("UTF-8"))
+                .andExpect(status().isBadRequest())
+                .andDo(print());
     }
 }
